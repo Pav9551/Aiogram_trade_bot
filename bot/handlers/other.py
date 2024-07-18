c29068ca6ba14115b57fff6ad3b98ecf55920b53
pav9551 <pav9551@yandex.ru>
2024-07-18 13:32:32 +0300

    add /bay

--- bot/handlers/other.py
+++ bot/handlers/other.py
@@ -31,10 +31,43 @@ async def __balance(msg: Message) -> None:
     # Парсим данные в словарь Python
     parsed_data = json.loads(json.dumps(AccountInfo))
     await bot.send_message(user.id, f"{user.username}, you balance: {parsed_data['balances'][0]['free']} {parsed_data['balances'][0]['asset']}")
+async def __buy(msg: Message) -> None:
+    bot: Bot = msg.bot
+    user = msg.from_user
 
+    market = mexc_spot_v3.mexc_market()
+
+    # Enter parameters in JSON format in the "params", for example: {"symbol":"BTCUSDT", "limit":"200"}
+    # If there are no parameters, no need to send params
+    params = {
+        "symbol": "KASUSDT",
+    }
+    Price = market.get_price(params)
+    print(Price)
+    trade = mexc_spot_v3.mexc_trade()
+
+    # Enter parameters in JSON format in the "params", for example: {"symbol":"BTCUSDT", "limit":"200"}
+    # If there are no parameters, no need to send params
+
+    AccountInfo = trade.get_account_info()
+    #print(AccountInfo)
+    # Парсим данные в словарь Python
+    parsed_data = json.loads(json.dumps(AccountInfo))
+    print(Price)
+    parsed_data = json.loads(json.dumps(Price))
+    current_course_of_KAS = float(parsed_data['price']) # курс KAS
+    current_course_of_KAS = 0.179 # курс KAS в момент покупки
+    quan_spent_USDT = 6 #потраченные USDT
+    quan_curr_KAS = quan_spent_USDT/current_course_of_KAS
+    profit = 0.3
+    quan_receive_USDT = quan_spent_USDT + quan_spent_USDT * profit/100
+
+
+    await bot.send_message(user.id, f"{user.username}, you Price: {current_course_of_KAS:10.6f} ,quan_curr_KAS: {quan_curr_KAS:5.2f}, quan_spent_USDT {quan_spent_USDT:5.6f} ,quan_receive_USDT: {quan_receive_USDT:5.6f}")
 def register_other_handlers(dp: Dispatcher) -> None:
     dp.register_message_handler(__get_id, commands=["id"])
     dp.register_message_handler(__start, commands=["start"])
     dp.register_message_handler(__balance, commands=["balance"])
+    dp.register_message_handler(__buy, commands=["buy"])
     dp.register_message_handler(other_messages, content_types=['text'], state=None)
 
