ac836c1946e48e5443b7cfa3f7f785ccd02f3140
pav9551 <pav9551@yandex.ru>
2024-10-18 12:40:10 +0300

    added example_listen_key.py

--- mexc_api/example_listen_key.py
+++ mexc_api/example_listen_key.py
@@ -1,10 +1,3 @@
 from mexc_api.mexcClass import mexc_trade
-query_parameters = {
-            'price': 0.1,
-            'quantity': 10,
-            'side': 'BUY',
-            'symbol': 'KASUSDT',
-            'type': 'LIMIT'
-        }
 mexc = mexc_trade()
 print(mexc.create_listen_key())
